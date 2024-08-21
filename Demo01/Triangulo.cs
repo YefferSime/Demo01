41204c7601de6c7a0061c1c3bfae578a7ef1ef12
YefferSime <138342642+YefferSime@users.noreply.github.com>
2024-08-20 21:17:04 -0500

    Update Triangulo.cs
    lab01

--- Demo01/Triangulo.cs
+++ Demo01/Triangulo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +8,40 @@ namespace Demo01
 {
     public class Triangulo
     {
-        public int Base { get; set; }
-        public int Altura { get; set; }
-        public int Area { get; set; }
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+        public int X3 { get; set; }
+        public int Y3 { get; set; }
+  
+        public double Perimetro { get; set; }
+        public double Area { get; set; }
 
-        public int CalcularArea()
+        private double CalcularDistancia(int x1, int y1, int x2, int y2)
         {
-            return (Base * Altura / 2);
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
 
-        public void MostrarArea()
+        
+        public void CalcularArea()
         {
-            Area = Base * Altura / 2;
+            double lado1 = CalcularDistancia(X1, Y1, X2, Y2);
+            double lado2 = CalcularDistancia(X2, Y2, X3, Y3);
+            double lado3 = CalcularDistancia(X3, Y3, X1, Y1);
+            double s = (lado1 + lado2 + lado3) / 2;
+            Area= Math.Sqrt(s * (s - lado1) * (s - lado2) * (s - lado3));
         }
+        public void CalcularPerimetro()
+        {
+            double lado1 = CalcularDistancia(X1, Y1, X2, Y2);
+            double lado2 = CalcularDistancia(X2, Y2, X3, Y3);
+            double lado3 = CalcularDistancia(X3, Y3, X1, Y1);
+            Perimetro = lado1 + lado2 + lado3;
+        }
+
+
+
 
     }
 }
