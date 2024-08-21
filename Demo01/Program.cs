935b3f43daddaa9320ee6fd45887f3ae20e10002
YefferSime <138342642+YefferSime@users.noreply.github.com>
2024-08-20 21:18:09 -0500

    lab01

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,49 +11,62 @@ namespace Demo01
         {
 
 
-            Persona persona = new Persona();
-            persona.Nombres = "Yeffer";
-            persona.Apellidos = "Sime";
-            persona.AsignarNombresCompletos();
+            //Persona persona = new Persona();
+            //persona.Nombres = "Yeffer";
+            //persona.Apellidos = "Sime";
+            //persona.AsignarNombresCompletos();
 
 
-            Persona persona2 = new Persona
+            // Persona persona2 = new Persona
+            //{
+            //    Nombres = "Dérek",
+            //     Apellidos = "Torrico",
+            //Edad=5
+            // };
+
+
+
+
+            // Estudiante estudiante = new Estudiante
+            // {
+            //     Nombres = "Yeffer",
+            //     Apellidos = "Sime",
+            //     Nota = 20
+            // };
+
+            // Profesor profesor = new Profesor
+            // {
+            //     Nombres = "Yeffer",
+            //     Apellidos = "Sime",
+            //     Sueldo = 1000
+            // };
+
+            // profesor.CalcularSueldoNeto();
+            // Console.WriteLine(profesor.SueldoNeto);
+
+
+            //Triangulo triangulo = new Triangulo();
+            //triangulo.Base = 12;
+            //triangulo.Altura = 13;
+            //triangulo.MostrarArea();
+
+            Triangulo triangulo = new Triangulo
             {
-                Nombres = "Dérek",
-                Apellidos = "Torrico",
-                //Edad=5
+                X1 = 0,
+                Y1 = 0,
+                X2 = 4, 
+                Y2 = 0,
+                X3 = 4,
+                Y3 = 3,
             };
-
-
-
-
-            Estudiante estudiante = new Estudiante
-            {
-                Nombres = "Yeffer",
-                Apellidos = "Sime",
-                Nota = 20
-            };
-
-            Profesor profesor = new Profesor
-            {
-                Nombres = "Yeffer",
-                Apellidos = "Sime",
-                Sueldo = 2000
-            };
-
-            profesor.CalcularSueldoNeto();
-            Console.WriteLine(profesor.SueldoNeto);
-
-
-            Triangulo triangulo = new Triangulo();
-            triangulo.Base = 12;
-            triangulo.Altura = 13;
-            triangulo.MostrarArea();
-
-            Console.WriteLine(persona.NombresCompletos) ;
+            triangulo.CalcularPerimetro();
+            triangulo.CalcularArea();
+            Console.WriteLine(triangulo.Perimetro) ;
             Console.WriteLine(triangulo.Area);
+            Console.WriteLine();
             Console.Read();
             
         }
     }
 }
+
